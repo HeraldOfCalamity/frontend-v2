56d3a95b038a2f8fdc6491266e3e141ca643757f
Leonardo Eguino <leonardoeguinorv@gmail.com>
2025-09-04 22:17:06 -0400

    feat: added environment variable for backend connection

--- src/config/benedetta.api.config.ts
+++ src/config/benedetta.api.config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = `http://localhost:8000`
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL as string;
 
 const api = axios.create({
     baseURL: `${BASE_URL}/`,
