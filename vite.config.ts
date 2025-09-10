ea5f280173abf41a24d163e7cd8fa945aae8436b
Leonardo Eguino <leonardoeguinorv@gmail.com>
2025-09-10 10:20:40 -0400

    feat!: created vercel.json and modifiedn vite config to solve redirection issue in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     allowedHosts: ['fc6cfb0e0aa4.ngrok-free.app']
   }
