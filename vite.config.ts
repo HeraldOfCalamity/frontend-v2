3782636ed9b0ad37d3990a8f2b17d2b41654ecc7
Leonardo Eguino <leonardoeguinorv@gmail.com>
2025-09-10 10:41:36 -0400

    feat: modified vercel and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   server: {
     allowedHosts: ['fc6cfb0e0aa4.ngrok-free.app']
   }
